1138266a82305582aba3cc6e3f5d9d04571bab64
Blax Fontaine <valerie.gehle@epitech.eu>
2020-05-07 19:08:44 +0200

    version 2.0

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,18 +1,3 @@
-// const initUpdateNavbarOnScroll = () => {
-//   const navbar = document.querySelector('.navbar');
-//   if (navbar) {
-//     window.addEventListener('scroll', () => {
-//         if (window.scrollY > 400) {
-//         navbar.classList.add('nav-black');
-//       } else {
-//         navbar.classList.remove('nav-black');
-//       }
-//     });
-//   }
-// }
-
-// initUpdateNavbarOnScroll();
-
 var w = Math.round($(document).width() / 255);
 var h = Math.round($(document).height() / 255);
 var body = $(".navbar-brand, .nav-item, .navbar-toggler-icon, .footer-link");
@@ -24,52 +9,29 @@ $(document).mousemove(function(e){
 });
 
 
-
-$(function() {
-    /**
-    * Smooth scrolling to page anchor on click
-    **/
-  $("a[href*='#']:not([href='#'])").click(function() {
-    event.preventDefault();
-    if (
-      location.hostname == this.hostname
-      && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
-      ) {
-        var anchor = $(this.hash);
-        anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
-        if ( anchor.length ) {
-          $("html, body").animate( { scrollTop: anchor.offset().top }, 1000).scrollIntoView(true);
-        }
-      }
+$(document).ready(function() {
+  $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+    var page = $(this).attr('href'); // Page cible
+    var speed = 750; // Durée de l'animation (en ms)
+    $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+    return false;
   });
 });
-
-
-
-
-const title = document.querySelector('.banner_title')
-
-//////// Light //////////
-document.onmousemove = function(e) {
-  let x = e.pageX - window.innerWidth/2;
-  let y = e.pageY - window.innerHeight/2;
-
-  title.style.setProperty('--x', x + 'px')
-  title.style.setProperty('--y', y + 'px')
-}
-
-////////////// Shadow ///////////////////
-title.onmousemove = function(e) {
-  let x = e.pageX - window.innerWidth/2;
-  let y = e.pageY - window.innerHeight/2;
-
-  let rad = Math.atan2(y, x).toFixed(2);
-  let length = Math.round(Math.sqrt((Math.pow(x,2))+(Math.pow(y,2)))/10);
-
-  let x_shadow = Math.round(length * Math.cos(rad));
-  let y_shadow = Math.round(length * Math.sin(rad));
-
-  title.style.setProperty('--x-shadow', - x_shadow + 'px')
-  title.style.setProperty('--y-shadow', - y_shadow + 'px')
-
-}
+// $(function() {
+//     /**
+//     * Smooth scrolling to page anchor on click
+//     **/
+//   $("a[href*='#']:not([href='#'])").click(function() {
+//     event.preventDefault();
+//     if (
+//       location.hostname == this.hostname
+//       && this.pathname.replace(/^\//,"") == location.pathname.replace(/^\//,"")
+//       ) {
+//         var anchor = $(this.hash);
+//         anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) +"]");
+//         if ( anchor.length ) {
+//           $("html, body").animate( { scrollTop: anchor.offset().top }, 1000).scrollIntoView(true);
+//         }
+//       }
+//   });
+// });
